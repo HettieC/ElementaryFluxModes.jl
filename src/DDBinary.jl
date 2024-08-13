ae04c26da7a0f93876156cfc0d2bdc26b45ee0bc
HettieC <hester.chapman@btinternet.com>
2024-08-13 09:56:53 +0200

    update doc

--- src/DDBinary.jl
+++ src/DDBinary.jl
@@ -33,8 +33,10 @@ $(TYPEDSIGNATURES)
 
 Implement the Double Description method in binary form.
 The input variables are:
--N: stoichiometric matrix, where all reaction directions have already been fixed 
-    so that reactions are in the forward direction
+-N: the stoichiometric matrix with only forward reactions. If any fluxes are fixed 
+    then N has the form [N1 w] where N1 is the stoichiometric matrix for non-fixed fluxes,
+    and w is the columns of fixed fluxes multiplied by their flux values.
+
 -K: initial nullspace of N, in the form [I;K*] 
 Output:
 -R: binary elementary flux modes 

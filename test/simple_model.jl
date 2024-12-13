3fe47233d4a2ba276ea65b5842f662eae772ad5b
HettieC <hester.chapman@btinternet.com>
2024-12-13 11:35:29 +0100

    diff efm doc

--- test/simple_model.jl
+++ test/simple_model.jl
@@ -5,14 +5,14 @@ const AC = AbstractFBCModels.CanonicalModel
 rs = [
     AC.Reaction(;
         name = "r1",
-        lower_bound = -2,
-        upper_bound = 0,
+        lower_bound = 0,
+        upper_bound = 1000,
         stoichiometry = Dict("m1" => 1.0),
     )
     AC.Reaction(;
         name = "r2",
-        lower_bound = -1,
-        upper_bound = 0,
+        lower_bound = 0,
+        upper_bound = 1000,
         stoichiometry = Dict("m2" => 1),
     )
     AC.Reaction(;
@@ -31,7 +31,7 @@ rs = [
     )
     AC.Reaction(;
         name = "r5",
-        lower_bound = -1000,
+        lower_bound = 0,
         upper_bound = 1000,
         stoichiometry = Dict("m2" => -1, "m4" => 1),
         gene_association_dnf = [["g4", "g5"]],
@@ -65,3 +65,37 @@ model = AC.Model(
     metabolites = Dict(x.name => x for x in ms),
     genes = Dict(x.name => x for x in gs),
 )
+
+gene_product_molar_masses = Dict(
+    "g1" => 1.0,
+    "g2" => 1.0,
+    "g3" => 1.0,
+    "g4" => 1.0,
+    "g5" => 1.0,
+)
+
+rid_kcat = Dict(
+    "r3" => 10.0,
+    "r4" => 10.0,
+    "r5" => 5.0
+)
+
+capacity = [
+    (
+        "pool_1",
+        [
+           "g1",
+           "g2",
+           "g3", 
+        ],
+        10.0,
+    ),
+    (
+        "pool_2",
+        [
+            "g4",
+            "g5",
+        ],
+        10.0,
+    ),
+]

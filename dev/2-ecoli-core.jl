f9881403a06a2e1c6a43e22d61cfa07c7caca571
Documenter.jl <documenter@juliadocs.github.io>
2024-12-17 10:59:38 +0000

    build based on 699a09f

--- dev/2-ecoli-core.jl
+++
@@ -1,9 +0,0 @@
-# # _E. coli_ core model
-
-using ElementaryFluxModes
-
-# It has been proven that enzyme constrained models with K constraints 
-# will use a maximum of K EFMs in their optimal solution (de Groot 2019 (**check))
-
-# Here, we take the _E. coli_ core model with two enzyme constraints, find the optimal 
-# solution, and then find the EFMs in the optimal solution.

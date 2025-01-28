2c0119bce820cf95f609510a627b663154c18301
HettieC <hester.chapman@btinternet.com>
2025-01-28 16:44:40 +0100

    tests deps

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,18 @@
 using ElementaryFluxModes
 using Test
 
+import AbstractFBCModels as A
+import AbstractFBCModels.CanonicalModel as CM
+using JSONFBCModels
+import FastDifferentiation as F
+const Ex = F.Node
+import DifferentiableMetabolism as D
+using COBREXA
+import COBREXA as X
+using JSON
+import Tulip as T
+using CairoMakie
+
 @testset "ElementaryFluxModes.jl" begin
     @testset "Binary implementation of double dispatch EFMs" begin
         include("ddbinary.jl")
